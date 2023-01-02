16dd1ea471582752bd3e5a395f791f1674443c49
Saket Kothari <saketkothari722@gmail.com>
2023-01-02 14:36:41 +0530

    using the RestTemplate to make a REST API call from the Spring controller directly to another microservice and unmarshall the response into an object instance.

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,18 +1,43 @@
 package io.javabrains.moviecatalogservice.resources;
 
 import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
+import io.javabrains.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(new CatalogItem("Transformers", "Test Desc", 4));
+
+        // RestTemplate is the utility object which makes this call
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        // Get all rated movie IDs (used the same Rating model)
+        // Hardcoded the ratings
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 5),
+                new Rating("5678", 4)
+        );
+
+        // for each rating creating a catalog element with hardcoded name and desc and making it as List
+        return ratings.stream().map(rating -> {
+            Movie movie =  restTemplate.getForObject("http://localhost:8082/movies/" +rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        }).collect(Collectors.toList());
+
+
+        // For each movie ID call movie infoService and get details
+
+        // Put them all together
     }
 }
