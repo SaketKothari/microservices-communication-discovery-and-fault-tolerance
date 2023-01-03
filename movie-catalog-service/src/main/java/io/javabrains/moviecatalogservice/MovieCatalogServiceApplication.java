32fc351d130b6145f14a5c73daa39bb6fdf4e7f8
Saket Kothari <saketkothari722@gmail.com>
2023-01-03 10:03:30 +0530

    switch to WebClient for making API calls

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -15,6 +16,11 @@ public class MovieCatalogServiceApplication {
 	public RestTemplate getRestTemplate() {
 		// RestTemplate is the utility object which makes this call
 		return new RestTemplate();
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 	public static void main(String[] args) {
--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +20,10 @@ public class MovieCatalogResource {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
@@ -31,7 +36,15 @@ public class MovieCatalogResource {
 
         // for each rating creating a catalog element with hardcoded name and desc and making it as List
         return ratings.stream().map(rating -> {
-            Movie movie =  restTemplate.getForObject("http://localhost:8082/movies/" +rating.getMovieId(), Movie.class);
+           // Movie movie =  restTemplate.getForObject("http://localhost:8082/movies/" +rating.getMovieId(), Movie.class);
+
+              Movie movie = webClientBuilder.build()
+                        .get()
+                        .uri("http://localhost:8082/movies/" +rating.getMovieId())
+                        .retrieve()
+                        .bodyToMono(Movie.class)
+                        .block();
+
             return new CatalogItem(movie.getName(), "Test", rating.getRating());
         }).collect(Collectors.toList());
 
